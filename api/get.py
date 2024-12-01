cfc37d707051186edb87aa2440230c0197fd5553
Anhelina Bodak <angelinabodak2@gmail.com>
2024-12-02 00:31:15 +0200

    Update1

--- api/get.py
+++ api/get.py
@@ -1,16 +1,13 @@
 from fastapi import APIRouter, Query
 import json
 
-# Create an APIRouter instance
 router = APIRouter(tags=['Search CVEs'])
 
-# Function to load the data from the JSON file
 def load_vulnerabilities(file_path='data/known_exploited_vulnerabilities.json'):
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
 
-# Function to search for CVEs containing the query keyword
 def search_cves_by_keyword(vulnerabilities, keyword):
     matching_cves = [
         vulnerability for vulnerability in vulnerabilities["vulnerabilities"]
@@ -18,13 +15,10 @@ def search_cves_by_keyword(vulnerabilities, keyword):
     ]
     return matching_cves
 
-# Function to handle the search endpoint
 @router.get("/get")
 def search_vulnerabilities(query: str = Query(..., description="Keyword to search in CVE data")):
-    # Load vulnerabilities data
     data = load_vulnerabilities()
 
-    # Search for CVEs containing the query keyword
     matching_vulnerabilities = search_cves_by_keyword(data, query)
     no_vulnerabilities = "No vulnerabilities found("
     if(len(matching_vulnerabilities) < 1): return no_vulnerabilities
